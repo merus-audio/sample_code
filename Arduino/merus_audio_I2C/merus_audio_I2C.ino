171d100f26ea4fd6a422c340227dd18dcea42c23
apps@merus <support@merus-audio.com>
2017-05-25 16:15:39 +0200

    interrupt implemented
    interrupt for error handling of temperature warning

--- Arduino/merus_audio_I2C/merus_audio_I2C.ino
+++ Arduino/merus_audio_I2C/merus_audio_I2C.ino
@@ -22,6 +22,7 @@
  * Revisions: 
  * 20170524: intial sample version
  * 20170514: tested version + comments 
+ * 20170524: device error on interrupt: check for temp warning
  *  
  * This code is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -32,14 +33,32 @@
 
 #include <I2C.h>                                // plug-in your MCU specific I2C library
 
+ /*-------- Variable declaration ---------*/
+  int gain;
+  int pmp_sel;
+  byte dev_err_acc;
+
 void setup ()
 {
   /* -- MERUS AUDIO device intialization --
    * 1) Gain at 26dB  
    * 2) PMP selection
+   * 3) Device error on interrupt
    */
-  
+ 
+  attachInterrupt (0, device_error, FALLING);   // attach interrupt handler 
+                                                // 0 is the internal interrupt attached to pin 2
+                                                // interrupt triggered on falling edge
+
   I2c.begin();                                  // begin I2C communication
+  
+  /* ------- Clear Error Handler --------
+   *  Error registers need to be cleared 
+   *  at start-up. This is done by toggle
+   *  eh_clear (0x2D) bit 2.
+   *  ---------------------------------- */
+  I2c.write(0x20,0x2D,0x34);                    // set eh_clear to 1
+  I2c.write(0x20,0x2D,0x30);                    // set eh_clear to 0
   
   /* ------- Select Analog Gain --------
    *  Analog gain can be set by setting 
@@ -52,7 +71,7 @@ void setup ()
    * new value: 0010 1000 (0x28) */
   I2c.write(0x20,0x27,0x28);                    // audio_in_mode_ext = 1
   
-  int gain = 26;                                // select gain: 20=20dB; 26=26dB
+  gain = 26;                                    // select gain: 20=20dB; 26=26dB
   
   switch (gain) {
     case 20:                                    // 20dB gain case (default)
@@ -79,7 +98,7 @@ void setup ()
    *  register PMprofile (0x1D) 
    *  ---------------------------------- */
 
-  int pmp_sel = 1;                              // PMP select: 0 = PMP0; 1=PMP1; 2=PMP2; 3=PMP3; 4=PMP4
+  pmp_sel = 1;                                  // PMP select: 0=PMP0; 1=PMP1; 2=PMP2; 3=PMP3; 4=PMP4
 
   switch (pmp_sel) {
     case 0:                                     // PMP 0 case (default)
@@ -118,17 +137,60 @@ void setup ()
     break;
 
     default:
-    // no valid selction
+    // no valid selection
     break;
   }
   
   I2c.end();                                    // stop I2C communication
-
-    
+      
 }  // end of setup
+
+ /* ---- Select Power Mode Profile ----
+   *  Power Mode Profile can be set in 
+   *  register PMprofile (0x1D) 
+   *  ---------------------------------- */
+void device_error ()                           // Interrupt Service Routine (ISR)
+{
+  I2c.begin();                                 // begin I2C communication
+  I2c.read(0x20,0x6D,1);                       // read accumulated device error vector
+  dev_err_acc = I2c.receive();
+  
+  /*---------- error vector ------------ 
+   *| b7 | b6 | b5 | b4 | b3 | b2 | b1 | b0 |
+   * b0 = fly-cap over-voltage error
+   * b1 = over-current protection
+   * b2 = PLL error
+   * b3 = PVDD under-voltage protection
+   * b4 = temperature warning (around 125 degrees celsius)
+   * b5 = temperature error (around 150 degrees celsius)
+   * b6 = pin-to-pin low impedance protection
+   * b7 = DC protection
+   -------------------------------------
+   check bit 4 for over-temperature
+   ------------------------------------ */
+   if (0x08 & dev_err_acc)
+   {
+      // handle over-temperature warning                                     
+   }
+   else
+   {
+      // handle other  
+   }
+
+   /* ------- Clear Error Handler --------
+   *  Error registers need to be cleared 
+   *  again. This is done by toggle
+   *  eh_clear (0x2D) bit 2.
+   *  ---------------------------------- */
+  I2c.write(0x20,0x2D,0x34);                   // set eh_clear to 1
+  I2c.write(0x20,0x2D,0x30);                   // set eh_clear to 0
+
+  I2c.end();                                   // stop I2C communication
+}  
 
 void loop ()
 {
+  // wait for interrupt
   // MCU other instructions
 } 
 
