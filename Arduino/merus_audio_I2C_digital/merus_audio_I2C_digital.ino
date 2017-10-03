6e0724176627b09a33ce1bf939e64e49438d01cd
apps@merus <support@merus-audio.com>
2017-10-03 12:57:03 +0200

    adaptation to fit Linkplay ref design
    includes volume control and change in default I2S config.

--- Arduino/merus_audio_I2C_digital/merus_audio_I2C_digital.ino
+++ Arduino/merus_audio_I2C_digital/merus_audio_I2C_digital.ino
@@ -6,7 +6,9 @@
  * 
  * Description:
  * This code demonstrates initial I2C communication 
- * using Arduino Uno together with MA devices
+ * using Arduino Uno together with MA devices - P version
+ * this means that the MA device will take a digital
+ * audio as input stream - I2S
  * 
  * Use:
  * The code uses I2C lib from Wayne Truchsess
@@ -21,9 +23,8 @@
  * Device address: 0x20
  * 
  * Revisions: 
- * 20170524: intial sample version
- * 20170514: tested version + comments 
- * 20170524: device error on interrupt: check for temp warning
+ * 20170814: intial version
+ * 20171003: Update for Linkplay ref board 
  *  
  * This code is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -35,21 +36,47 @@
 #include <I2C.h>                                // plug-in your MCU specific I2C library
 
  /*-------- Variable declaration ---------*/
-  int gain;
   int pmp_sel;
-  byte dev_err_acc;
+  int curr_vol;
 
+  int enable_n = 7;                             // /ENABLE connected to PD6
+  int mute_n = 6;                               // /MUTE connected tp PD7
+  // note schematic mute enable naming bug
+  int msel0 = 5;                                // MSEL0 is connected to PD5
+  
 void setup ()
 {
+  pinMode(enable_n, OUTPUT);
+  pinMode(mute_n, OUTPUT);
+  pinMode(msel0, OUTPUT);
+  
   /* -- MERUS AUDIO device intialization --
    * 1) Gain at 26dB  
    * 2) PMP selection
    * 3) Device error on interrupt
    */
  
-  attachInterrupt (0, device_error, FALLING);   // attach interrupt handler 
-                                                // 0 is the internal interrupt attached to pin 2
+  attachInterrupt (1, volume_up, FALLING);   // attach interrupt handler 
+                                                // 1 is the internal interrupt attached to pin 3
                                                 // interrupt triggered on falling edge
+
+  attachInterrupt (0, volume_down, FALLING);   // attach interrupt handler 
+                                                // 1 is the internal interrupt attached to pin 3
+                                                // interrupt triggered on falling edge
+                                                  
+  /* ---- Set output configuration -----
+   *  At start up keep device in reset 
+   *  in order to set mode selection
+   *  for SE, BTL, or PBTL
+   *  After mode selction enable device
+   * ----------------------------------- */
+   digitalWrite(mute_n, LOW);
+   digitalWrite(enable_n, HIGH);
+   digitalWrite(msel0, HIGH);
+   delay(100);
+   digitalWrite(enable_n, LOW);
+   delay(100); 
+  
 
   I2c.begin();                                  // begin I2C communication
   
@@ -61,45 +88,22 @@ void setup ()
   I2c.write(0x20,0x2D,0x34);                    // set eh_clear to 1
   I2c.write(0x20,0x2D,0x30);                    // set eh_clear to 0
   
-  /* ------- Select Analog Gain --------
-   *  Analog gain can be set by setting 
-   *  register audio_in_mode_ext (0x27) 
-   *  and register audio_in_mode (0x25)
+  /* ------- Set I2S config--- --------
+   *  
    *  ---------------------------------- */
+  I2c.write(0x20,0x35,0x08);                    // set i2s dataformat and enable processor
+  I2c.write(0x20,0x36,0x11);                    // set 32fs bitclock for 16bits data
+  I2c.write(0x20,0x40,0x54);                    // -60dB master volume
   
-  /* set audio_in_mode_ext (0x27)
-   * default:   0000 1000 (0x08)
-   * new value: 0010 1000 (0x28) */
-  I2c.write(0x20,0x27,0x28);                    // audio_in_mode_ext = 1
   
-  gain = 26;                                    // select gain: 20=20dB; 26=26dB
-  
-  switch (gain) {
-    case 20:                                    // 20dB gain case (default)
-    /* set audio_in_mode (0x25)
-    * default:   0001 0000 (0x10)
-    * new value: 0001 0000 (0x10) */
-    I2c.write(0x20,0x25,0x10);                  // audio_in_mode = 0
-    break;
-    
-    case 26:                                    // 26dB gain case 
-    /* set audio_in_mode (0x25)
-    * default:   0001 0000 (0x10)
-    * new value: 0011 0000 (0x30) */
-    I2c.write(0x20,0x25,0x30);                  // audio_in_mode = 1
-    break;
-
-    default:
-    // no valid selection
-    break;
-  }
+ 
 
   /* ---- Select Power Mode Profile ----
    *  Power Mode Profile can be set in 
    *  register PMprofile (0x1D) 
    *  ---------------------------------- */
 
-  pmp_sel = 1;                                  // PMP select: 0=PMP0; 1=PMP1; 2=PMP2; 3=PMP3; 4=PMP4
+  pmp_sel = 4;                                  // PMP select: 0=PMP0; 1=PMP1; 2=PMP2; 3=PMP3; 4=PMP4
 
   switch (pmp_sel) {
     case 0:                                     // PMP 0 case (default)
@@ -143,6 +147,8 @@ void setup ()
   }
   
   I2c.end();                                    // stop I2C communication
+  delay(100); 
+  digitalWrite(mute_n, HIGH); 
       
 }  // end of setup
 
@@ -150,43 +156,30 @@ void setup ()
    *  Power Mode Profile can be set in 
    *  register PMprofile (0x1D) 
    *  ---------------------------------- */
-void device_error ()                           // Interrupt Service Routine (ISR)
+void volume_up ()                           // Interrupt Service Routine (ISR)
 {
   I2c.begin();                                 // begin I2C communication
-  I2c.read(0x20,0x6D,1);                       // read accumulated device error vector
-  dev_err_acc = I2c.receive();
+  I2c.read(0x20,0x40,1);                       // read current volume
   
-  /*---------- error vector ------------ 
-   *| b7 | b6 | b5 | b4 | b3 | b2 | b1 | b0 |
-   * b0 = fly-cap over-voltage error
-   * b1 = over-current protection
-   * b2 = PLL error
-   * b3 = PVDD under-voltage protection
-   * b4 = temperature warning (around 125 degrees celsius)
-   * b5 = temperature error (around 150 degrees celsius)
-   * b6 = pin-to-pin low impedance protection
-   * b7 = DC protection
-   -------------------------------------
-   check bit 4 for over-temperature
-   ------------------------------------ */
-   if (0x08 & dev_err_acc)
-   {
-      // handle over-temperature warning                                     
-   }
-   else
-   {
-      // handle other  
-   }
+  curr_vol = I2c.receive();
+  curr_vol = curr_vol - 1;
+  
+  I2c.write(0x20,0x40,curr_vol);               // write new volume
 
-   /* ------- Clear Error Handler --------
-   *  Error registers need to be cleared 
-   *  again. This is done by toggle
-   *  eh_clear (0x2D) bit 2.
-   *  ---------------------------------- */
-  I2c.write(0x20,0x2D,0x34);                   // set eh_clear to 1
-  I2c.write(0x20,0x2D,0x30);                   // set eh_clear to 0
+  I2c.end();                                   // stop I2C communication   
+}  
 
-  I2c.end();                                   // stop I2C communication
+void volume_down ()                           // Interrupt Service Routine (ISR)
+{
+  I2c.begin();                                 // begin I2C communication
+  I2c.read(0x20,0x40,1);                       // read current volume
+  
+  curr_vol = I2c.receive();
+  curr_vol = curr_vol + 1;
+  
+  I2c.write(0x20,0x40,curr_vol);               // write new volume
+
+  I2c.end();                                   // stop I2C communication   
 }  
 
 void loop ()
